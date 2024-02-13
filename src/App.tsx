123596c000bda6094f1389786f133f0cabc4bff4
Andrey Belomyttsev <andymedtech@gmail.com>
2024-02-13 23:07:51 +0300

    add CSS for react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 import BalancePage from "./pages/BalancePage";
 import IncomesPage from "./pages/IncomesPage";
 import ExpensesPage from "./pages/ExpensesPage";
-// import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <>
-      {/* <h1>Project Finance</h1> */}
       <header>
-        {/* <Link to="/">home</Link> */}
-        <Link to="/balance">Баланс</Link>
-        <Link to="/incomes">Доходы</Link>
-        <Link to="/expenses">Расходы</Link>
+        <NavLink className={""} to="/balance">
+          Баланс
+        </NavLink>
+        <NavLink className={""} to="/incomes">
+          Доходы
+        </NavLink>
+        <NavLink className={""} to="/expenses">
+          Расходы
+        </NavLink>
       </header>
       <Routes>
         <Route path="/*" element={<BalancePage />} />
         <Route path="/incomes" element={<IncomesPage />} />
         <Route path="/expenses" element={<ExpensesPage />} />
-        {/* <Route path="/*" element={<NotFoundPage />} /> */}
       </Routes>
-      <footer>2024</footer>
+      <footer></footer>
     </>
   );
 }
